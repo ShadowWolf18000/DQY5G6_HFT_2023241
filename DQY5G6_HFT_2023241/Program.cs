c38aa836b7a35864c03226a306693087bf70700e
mihal <mihal@DESKTOP-C603H77>
2023-10-16 13:41:04 +0200

    Add project files.

---
+++ DQY5G6_HFT_2023241/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DQY5G6_HFT_2023241
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
