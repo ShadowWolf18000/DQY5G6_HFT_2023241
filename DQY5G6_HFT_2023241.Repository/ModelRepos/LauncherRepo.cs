638ec66c6253ac10299168f2cc0c9fb0ca31ccb6
mihalybalint <mihalybalint256@gmail.com>
2023-11-05 21:39:07 +0100

    LauncherRepository completed.

--- DQY5G6_HFT_2023241.Repository/ModelRepos/LauncherRepo.cs
+++ DQY5G6_HFT_2023241.Repository/ModelRepos/LauncherRepo.cs
@@ -9,5 +9,21 @@ namespace DQY5G6_HFT_2023241.Repository
 {
     internal class LauncherRepo : GenericRepository<Launcher>, IRepository<Launcher>
     {
+        public LauncherRepo(GameDbContext ctx) : base(ctx) { }
+
+        public override Launcher Read(int id)
+        {
+            return gameDbContext.Launchers.FirstOrDefault(l => l.LauncherID == id);
+        }
+
+        public override void Update(Launcher launcher)
+        {
+            var old = Read(launcher.LauncherID);
+            foreach (var property in old.GetType().GetProperties())
+            {
+                property.SetValue(old, property.GetValue(launcher));
+            }
+            gameDbContext.SaveChanges();
+        }
     }
 }
