d3ee8ac3c110de6aa3ff3aec6b225bb668963d7d
mihalybalint <mihalybalint256@gmail.com>
2023-11-05 21:35:47 +0100

    GameRepository completed.

--- DQY5G6_HFT_2023241.Repository/ModelRepos/GameRepo.cs
+++ DQY5G6_HFT_2023241.Repository/ModelRepos/GameRepo.cs
@@ -9,5 +9,21 @@ namespace DQY5G6_HFT_2023241.Repository
 {
     internal class GameRepo : GenericRepository<Game>, IRepository<Game>
     {
+        public GameRepo(GameDbContext ctx) : base(ctx) { }
+
+        public override Game Read(int id)
+        {
+            return gameDbContext.Games.FirstOrDefault(g => g.GameID == id);
+        }
+
+        public override void Update(Game game)
+        {
+            var old = Read(game.GameID);
+            foreach (var property in old.GetType().GetProperties())
+            {
+                property.SetValue(old, property.GetValue(game));
+            }
+            gameDbContext.SaveChanges();
+        }
     }
 }
