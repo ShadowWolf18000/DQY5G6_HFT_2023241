7eb0ef8f77e5a32745a5fdf10e4eab9dbc81cdee
mihalybalint <mihalybalint256@gmail.com>
2023-11-22 10:52:18 +0100

    DeveloperLogic fully completed

--- DQY5G6_HFT_2023241.Logic/Classes/DeveloperLogic.cs
+++ DQY5G6_HFT_2023241.Logic/Classes/DeveloperLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DQY5G6_HFT_2023241.Models;
+using DQY5G6_HFT_2023241.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,38 @@ using System.Threading.Tasks;
 
 namespace DQY5G6_HFT_2023241.Logic
 {
-    internal class DeveloperLogic
+    internal class DeveloperLogic : IDeveloperLogic
     {
+        IRepository<Developer> repository;
 
+        public DeveloperLogic(IRepository<Developer> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(Developer dev)
+        {
+            repository.Create(dev);
+        }
+
+        public void Delete(int id)
+        {
+            repository.Delete(id);
+        }
+
+        public Developer Read(int id)
+        {
+            return repository.Read(id);
+        }
+
+        public IQueryable<Developer> ReadAll()
+        {
+            return repository.ReadAll();
+        }
+
+        public void Update(Developer dev)
+        {
+            repository.Update(dev);
+        }
     }
 }
