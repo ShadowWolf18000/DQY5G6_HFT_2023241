8c0029c9efc38e754c56e5bc3e675a16a2308224
mihalybalint <mihalybalint256@gmail.com>
2023-11-22 10:50:35 +0100

    GameLogic fully completed

--- DQY5G6_HFT_2023241.Logic/Classes/GameLogic.cs
+++ DQY5G6_HFT_2023241.Logic/Classes/GameLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DQY5G6_HFT_2023241.Models;
+using DQY5G6_HFT_2023241.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace DQY5G6_HFT_2023241.Logic
 {
-    internal class GameLogic
+    internal class GameLogic : IGameLogic
     {
+        IRepository<Game> repository;
+
+        public GameLogic(IRepository<Game> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(Game game)
+        {
+            repository.Create(game);
+        }
+
+        public void Delete(int id)
+        {
+            repository.Delete(id);
+        }
+
+        public Game Read(int id)
+        {
+            return repository.Read(id);
+        }
+
+        public IQueryable<Game> ReadAll()
+        {
+            return repository.ReadAll();
+        }
+
+        public void Update(Game game)
+        {
+            repository.Update(game);
+        }
     }
 }
