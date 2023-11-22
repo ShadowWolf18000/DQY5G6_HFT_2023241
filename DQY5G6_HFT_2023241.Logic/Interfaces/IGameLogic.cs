0f50ad7bb914d7fd87ca4a4ca877da03bfe8fa1b
mihalybalint <mihalybalint256@gmail.com>
2023-11-22 10:37:17 +0100

    Interface for Game logic completed

--- DQY5G6_HFT_2023241.Logic/Interfaces/IGameLogic.cs
+++ DQY5G6_HFT_2023241.Logic/Interfaces/IGameLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DQY5G6_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace DQY5G6_HFT_2023241.Logic.Interfaces
 {
     internal interface IGameLogic
     {
-
+        void Create(Game dev);
+        void Delete(int id);
+        Game Read(int id);
+        IQueryable<Game> ReadAll();
+        void Update(Game dev);
     }
 }
