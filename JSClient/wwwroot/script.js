01ed555c804819a6d7dde22ac9434242c7fa33b2
Mihaly Balint <mihalybalint256@gmail.com>
2024-05-15 19:37:59 +0200

    All CRUD done in script

--- JSClient/wwwroot/script.js
+++ JSClient/wwwroot/script.js
@@ -30,6 +30,7 @@ async function getLaunchers() {
         .then(y => {
             launchers = y;
             console.log("launchers GET successful");
+            console.log(launchers);
         });
 }
 
@@ -172,13 +173,14 @@ let developerUpdateId = 0;
 
 function displayDevelopers() {
     document.getElementById('developerwindow').style.display = 'flex';
-    //document.getElementById('launcherwindow').style.display = 'none';
-    //document.getElementById('gamewindow').style.display = 'none';
+    document.getElementById('launcherwindow').style.display = 'none';
+    document.getElementById('gamewindow').style.display = 'none';
 
     document.getElementById('updateDeveloper').style.display = 'none';
-    document.getElementById('developersByLauncherWindow').style.display = 'none';
-    //document.getElementById('updateLauncher').style.display = 'none';
-    //document.getElementById('updateGame').style.display = 'none';
+    document.getElementById('developersByLauncherWindow').style.display = 'flex';
+
+    document.getElementById('updateLauncher').style.display = 'none';
+    document.getElementById('updateGame').style.display = 'none';
 
     getDevelopers();
     listDevelopers();
@@ -198,32 +200,36 @@ function listDevelopers() {
     });
 }
 
-function addDeveloper() {
+async function addDeveloper() {
     let developerID = document.getElementById('developerid').value;
     let developerName = document.getElementById('developername').value;
     let foundingYear = document.getElementById('foundingyear').value;
-
-    fetch('http://localhost:37523/developer', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            developerID: developerID,
-            developerName: developerName,
-            foundingYear: foundingYear
+    if (doesDeveloperExist(developerID) == false) {
+        fetch('http://localhost:37523/developer', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                developerID: developerID,
+                developerName: developerName,
+                foundingYear: foundingYear
+            })
         })
-    })
-    .then(x => {
-        console.log(x);
-        displayDevelopers();
-    })
-    .catch((error) => {
-        console.error("Error:", error);
-    });
+            .then(x => {
+                console.log(x);
+                displayDevelopers();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
+    else {
+        console.log("Error: cannot add Developer, because Developer with the same id already exists in the database.");
+    }
 }
 
-async function removeDeveloper(id){
+async function removeDeveloper(id) {
     await fetch('http://localhost:37523/developer/' + id, {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
@@ -277,10 +283,286 @@ function showUpdateDeveloper(developerID, developerName, foundingYear) {
     document.getElementById('foundingyearUpdate').value = foundingYear;
     developerUpdateId = developerID;
 }
+
+function doesDeveloperExist(id) {
+    let k = false;
+    let i = 0;
+    while (k != true && i < developers.length) {
+        k = developers[i].developerID == id;
+        i++;
+    }
+    return k;
+}
 //#endregion
 
+// Launchers
+//#region Launchers
+let launcherUpdateId = 0;
 
+function displayLaunchers() {
+    document.getElementById('developerwindow').style.display = 'none';
+    document.getElementById('launcherwindow').style.display = 'flex';
+    document.getElementById('gamewindow').style.display = 'none';
 
+    document.getElementById('updateDeveloper').style.display = 'none';
+    document.getElementById('developersByLauncherWindow').style.display = 'none';
+
+    document.getElementById('updateLauncher').style.display = 'none';
+    document.getElementById('updateGame').style.display = 'none';
+
+    getLaunchers();
+    listLaunchers();
+}
+
+function listLaunchers() {
+    document.getElementById('launchers').innerHTML = "";
+    launchers.forEach(t => {
+        document.getElementById('launchers').innerHTML +=
+            `<tr>
+                    <td><input type="radio" name="selectLauncherRadio" onclick='showUpdateLauncher("${t.launcherID}", "${t.launcherName}", "${t.owner}")'></input></td>
+                    <td>${t.launcherID}</td> 
+                    <td>${t.launcherName}</td>
+                    <td>${t.owner}</td>
+                    <td><button type="button" onclick='removeLauncher(${t.launcherID})'>Delete</button></td>
+                </tr>`;
+    });
+}
+
+async function addLauncher() {
+    let launcherID = document.getElementById('launcherid').value;
+    let launcherName = document.getElementById('launchername').value;
+    let owner = document.getElementById('owner').value;
+    if (doesLauncherExist(launcherID) == false) {
+        fetch('http://localhost:37523/launcher', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                launcherID: launcherID,
+                launcherName: launcherName,
+                owner: owner
+            })
+        })
+            .then(x => {
+                console.log(x);
+                displayLaunchers();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
+    else {
+        console.log("Error: cannot add Launcher, because Launcher with the same id already exists in the database.");
+    }
+}
+
+async function removeLauncher(id) {
+    await fetch('http://localhost:37523/launcher/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: null
+    }).then(data => {
+        console.log(data);
+        console.log("launcher DELETE successful");
+        return getLaunchers();
+    })
+        .then(() =>
+            displayLaunchers())
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function updateLauncher() {
+    let launcherID = document.getElementById('launcheridUpdate').value;
+    let launcherName = document.getElementById('launchernameUpdate').value;
+    let owner = document.getElementById('ownerUpdate').value;
+
+    fetch('http://localhost:37523/launcher', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            launcherID: launcherID,
+            launcherName: launcherName,
+            owner: owner
+        })
+    })
+        .then(data => {
+            console.log(data);
+            return getLaunchers();
+        })
+        .then(() => {
+            displayLaunchers();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+    document.getElementById('updateLauncher').style.display = 'none';
+}
+
+function showUpdateLauncher(launcherID, launcherName, Owner) {
+    document.getElementById('updateLauncher').style.display = 'flex';
+    document.getElementById('launcheridUpdate').value = launcherID;
+    document.getElementById('launchernameUpdate').value = launcherName;
+    document.getElementById('ownerUpdate').value = Owner;
+    launcherUpdateId = launcherID;
+}
+
+function doesLauncherExist(id) {
+    let k = false;
+    let i = 0;
+    while (k != true && i < launchers.length) {
+        k = launchers[i].launcherID == id;
+        i++;
+    }
+    return k;
+}
+//#endregion
+
+// Games
+// #region Games
+let gameUpdateId = 0;
+
+function displayGames() {
+    document.getElementById('developerwindow').style.display = 'none';
+    document.getElementById('launcherwindow').style.display = 'none';
+    document.getElementById('gamewindow').style.display = 'flex';
+
+    document.getElementById('updateDeveloper').style.display = 'none';
+    document.getElementById('developersByLauncherWindow').style.display = 'none';
+
+    document.getElementById('updateLauncher').style.display = 'none';
+    document.getElementById('updateGame').style.display = 'none';
+
+    getGames();
+    listGames();
+}
+
+function listGames() {
+    document.getElementById('games').innerHTML = "";
+    games.forEach(t => {
+        document.getElementById('games').innerHTML +=
+            `<tr>
+                    <td><input type="radio" name="selectGameRadio" onclick='showUpdateGame("${t.gameID}", "${t.title}", "${t.developerID}", "${t.launcherID}", "${t.rating}")'></input></td>
+                    <td>${t.gameID}</td> 
+                    <td>${t.title}</td>
+                    <td>${t.developerID}</td>
+                    <td>${t.launcherID}</td>
+                    <td>${t.rating}</td>
+                    <td><button type="button" onclick='removeGame(${t.gameID})'>Delete</button></td>
+            </tr>`;
+    });
+}
+
+async function addGame() {
+    let gameID = document.getElementById('gameid').value;
+    let title = document.getElementById('gametitle').value;
+    let developerID = document.getElementById('game_developerid').value;
+    let launcherID = document.getElementById('game_launcherid').value;
+    let rating = document.getElementById('rating').value;
+
+    if (doesGameExist(gameID) == false) {
+        fetch('http://localhost:37523/game', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                gameID: gameID,
+                title: title,
+                developerID: developerID,
+                launcherID: launcherID,
+                rating: rating
+            })
+        })
+            .then(x => {
+                console.log(x);
+                displayGames();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
+    else {
+        console.log("Error: cannot add Game, because Game with the same id already exists in the database.");
+    }
+}
+
+async function removeGame(id) {
+    await fetch('http://localhost:37523/game/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: null
+    }).then(data => {
+        console.log(data);
+        console.log("game DELETE successful");
+        return getGames();
+    })
+        .then(() =>
+            displayGames())
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function updateGame() {
+    let gameID = document.getElementById('gameidUpdate').value;
+    let title = document.getElementById('gametitleUpdate').value;
+    let developerID = document.getElementById('game_developeridUpdate').value;
+    let launcherID = document.getElementById('game_launcheridUpdate').value;
+    let rating = document.getElementById('ratingUpdate').value;
+
+    fetch('http://localhost:37523/game', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            gameID: gameID,
+            title: title,
+            developerID: developerID,
+            launcherID: launcherID,
+            rating: rating
+        })
+    })
+        .then(data => {
+            console.log(data);
+            return getGames();
+        })
+        .then(() => {
+            displayGames();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+    document.getElementById('updateGame').style.display = 'none';
+}
+
+function showUpdateGame(gameID, title, developerID, launcherID, rating) {
+    document.getElementById('updateGame').style.display = 'flex';
+    document.getElementById('gameidUpdate').value = gameID;
+    document.getElementById('gametitleUpdate').value = title;
+    document.getElementById('game_developeridUpdate').value = developerID;
+    document.getElementById('game_launcheridUpdate').value = launcherID;
+    document.getElementById('ratingUpdate').value = rating;
+    gameUpdateId = gameID;
+}
+
+function doesGameExist(id) {
+    let k = false;
+    let i = 0;
+    while (k != true && i < games.length) {
+        k = games[i].gameID == id;
+        i++;
+    }
+    return k;
+}
+// #endregion
 
 
 
